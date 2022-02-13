cdfb31c2a435f8bf62d6d26aabad532aa65139dd
Robin <Robinyyy@t-online.de>
2022-02-13 19:20:20 +0100

    added shared parameter option for rtac

--- RTAC.py
+++ RTAC.py
@@ -1,6 +1,7 @@
 from vacuum import VacuumEnv
 import probeEnvironments
-from network import PolicyNetwork, ValueNetwork
+from simp_lunar_lander import SimpLunarLander
+from network import PolValModule
 from utilities import ReplayBuffer
 import rtmdp
 import numpy as np
@@ -16,11 +17,14 @@ import time
 
 class RTAC:
     
-    def __init__(self,env, eval_env = None, alpha = 0.2, gamma = 0.99, lr_pol = 0.00003, lr_val = 0.0003, buffer_size = 10000, batch_size = 256, hidden_size = 256, num_hidden = 2):
+    #beta = actor-critic loss factor
+    #gamma = discount factor
+    #alpha = entropy scale
+    def __init__(self,env, eval_env = None, alpha = 0.2, gamma = 0.99, lr = 0.0003, beta = 0.1 , buffer_size = 10000, batch_size = 256, hidden_size = 256, num_hidden = 2, shared_parameters = False):
         self.ALPHA = alpha
+        self.BETA = beta
         self.GAMMA = gamma
-        self.LR_POL = lr_pol
-        self.LR_VAL = lr_val
+        self.LR = lr
         self.BUFFER_SIZE = buffer_size
         self.BATCH_SIZE = batch_size
 
@@ -31,15 +35,13 @@ class RTAC:
         self.eval_env = eval_env
         self.nom_actions = env.action_space.n
     
-        self.pol_network = PolicyNetwork(env.observation_space[0].shape[0] + env.action_space.n,env.action_space.n, hidden_size = hidden_size, num_hidden = num_hidden)
-        self.val_network = ValueNetwork(env.observation_space[0].shape[0] + env.action_space.n, hidden_size = hidden_size, num_hidden = num_hidden)
+        self.shared_parameters = shared_parameters
+        self.network = PolValModule(env.observation_space[0].shape[0] + env.action_space.n,env.action_space.n, hidden_size = hidden_size, num_hidden = num_hidden, shared_parameters = shared_parameters)
+
+        self.optim = optim.Adam(self.network.parameters(), lr=lr)
+        self.mse_loss = nn.MSELoss()
         
         self.replay = ReplayBuffer(buffer_size)
-
-        self.optim_pol = optim.Adam(self.pol_network.parameters(), lr=lr_pol)
-        self.optim_val = optim.Adam(self.val_network.parameters(), lr=lr_val)
-
-        self.mse_loss = nn.MSELoss()
 
     """
         Loads the model with parameters contained in the files in the 
@@ -48,8 +50,7 @@ class RTAC:
         checkpoint: Absolute path without ending to the two files the model is saved in.
     """
     def retrieve_model(self,checkpoint):
-        self.pol_network.load_state_dict(torch.load("{0}.pol".format(checkpoint)))
-        self.val_network.load_state_dict(torch.load("{0}.val".format(checkpoint)))
+        self.network.load_state_dict(torch.load("{0}.model".format(checkpoint)))
         print("Continuing training on {0}.".format(checkpoint))
 
     """
@@ -58,8 +59,7 @@ class RTAC:
         log_dest: Absolute path without ending to the two files the model is to be saved in.
     """
     def save_model(self,log_dest):
-        torch.save(self.pol_network.state_dict(),"{0}.pol".format(log_dest))
-        torch.save(self.val_network.state_dict(),"{0}.val".format(log_dest))
+        torch.save(self.network.state_dict(),"{0}.model".format(log_dest))
         print("Saved current training progress")
 
     """
@@ -102,14 +102,14 @@ class RTAC:
                 Perform step on env and add step data to replay buffer
                 """
                 state = flatten_rtmdp_obs(state,self.nom_actions)
-                action = self.pol_network.act(state)
+                action = self.network.act(state)
                 next_state,reward,done,_ = self.env.step(action)
                 self.replay.add_data( (torch.tensor(state),reward,torch.tensor(next_state[0]),done) )
                 state = next_state
                 
                 env_steps += 1
-                if env_steps % 100 == 0 and self.eval_env is not None:
-                    avg_rew = evaluate_policy(self.pol_network.act,self.eval_env)
+                if env_steps % 100 == 0 and self.eval_env is not None and self.replay.capacity_reached():
+                    avg_rew = evaluate_policy(self.network.act,self.eval_env)
                     print("Policy gathered a average reward of {0}".format(avg_rew))
                 
                 """
@@ -121,14 +121,14 @@ class RTAC:
                     """
                     sample = np.array(self.replay.sample(self.BATCH_SIZE),dtype=object)
                     states = torch.stack(list(sample[:,0]), dim=0) #tensor of states
-                    distributions = self.pol_network.get_action_distribution(states) #tensor of action distributions on states
+                    distributions = self.network.get_action_distribution(states) #tensor of action distributions on states
                     rewards = torch.tensor(np.array(sample[:,1],dtype=float)) #reward tensor
                     next_states_obs = torch.stack(list(sample[:,2]), dim=0) #tensor of next states without the action part
                     dones = torch.tensor(np.array(sample[:,3],dtype=float))
                     
                     values_next_states = []
                     for j in range(self.nom_actions):
-                        values_next_states.append(self.val_network(torch.tensor([flatten_rtmdp_obs( (list(next_states_obs[i]),j),self.nom_actions  ) for i in range(self.BATCH_SIZE)])))
+                        values_next_states.append(self.network.get_value(torch.tensor([flatten_rtmdp_obs( (list(next_states_obs[i]),j),self.nom_actions  ) for i in range(self.BATCH_SIZE)])))
                     values_next_states = torch.squeeze(torch.stack(values_next_states,dim=1),dim=2) #tensor of values of next states with all actions
                
                     """
@@ -136,27 +136,24 @@ class RTAC:
                     """
                     #calc value func loss
                     targets = (rewards + torch.sum(distributions * ( (1 - dones[:,None]) * self.GAMMA * values_next_states - self.ALPHA * distributions.log() ),1)).detach().float()
-                    values = torch.squeeze(self.val_network(states),1)
+                    values = torch.squeeze(self.network.get_value(states),1)
                     loss_value = self.mse_loss(values,targets)
                     
                     #calc policy func loss
                     loss_pol = torch.sum(distributions * (distributions.log() - (1-dones[:,None]) * self.GAMMA*(1/self.ALPHA)* (values_next_states.detach())),1).mean()
                     
+                    loss = self.BETA * loss_pol + (1 - self.BETA) * loss_value
                     """
                     Update parameters
                     """
-                    self.optim_pol.zero_grad()
-                    loss_pol.backward()
-                    self.optim_pol.step()
-                    
-                    self.optim_val.zero_grad()
-                    loss_value.backward()
-                    self.optim_val.step() 
+                    self.optim.zero_grad()
+                    loss.backward()
+                    self.optim.step()
 
-
-
-#env = rtmdp.RTMDP(gym.make('CartPole-v1'),0)
-#eval_env = rtmdp.RTMDP(gym.make('CartPole-v1'),0)
-#test = RTAC(env,eval_env)
-#test.train()
+"""
+env = rtmdp.RTMDP(gym.make('CartPole-v1'),0)
+eval_env = rtmdp.RTMDP(gym.make('CartPole-v1'),0)
+test = RTAC(env,eval_env)
+test.train()
+"""
 
